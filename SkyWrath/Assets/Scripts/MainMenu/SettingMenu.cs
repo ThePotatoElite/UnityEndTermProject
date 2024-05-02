2d3756a579591320711e236146785672d87f5bb6
Diasiris123 <ilia.tyupa@gmail.com>
2024-05-02 23:43:04 +0300

    Settings Menu

--- SkyWrath/Assets/Scripts/MainMenu/SettingMenu.cs
+++ SkyWrath/Assets/Scripts/MainMenu/SettingMenu.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioMixer MasterMixer;
+    public AudioMixer SFXMixer;
+    public AudioMixer NusicMixer;
+    public TMP_Dropdown ResolutionDropDown;
+    public TMP_Dropdown GraphicsDropDown;
+
+    Resolution[] resolutions;
+    private void Start()
     {
-        
+        List<string> options = new List<string>();
+        int currentResolutionIndex = 0;
+
+        resolutions = Screen.resolutions;
+        ResolutionDropDown.ClearOptions();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " X " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+                    currentResolutionIndex = i;
+        }
+
+        ResolutionDropDown.AddOptions(options);
+        ResolutionDropDown.value = currentResolutionIndex;
+        ResolutionDropDown.RefreshShownValue();
+
+        QualitySettings.SetQualityLevel(2);
+        GraphicsDropDown.value = 2;
+        GraphicsDropDown.RefreshShownValue();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetResolution(int ResolutionIndex)
     {
-        
+        Resolution resolution = resolutions[ResolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void SetVolume (float volume)
+    {
+
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
